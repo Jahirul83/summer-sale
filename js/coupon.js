2731005b25f43de56235d53dae3a842e59cb7880
Jahirul83 <jahirulislamcse83.com>
2023-08-20 19:48:23 +0600

    coupon js added

---
+++ js/coupon.js
@@ -0,0 +1,17 @@
+document.getElementById('coupon-input').addEventListener('keyup',function(event){
+
+
+    const text = event.target.value;
+    const applyBtn = document.getElementById('coupon-apply-btn');
+    const couponText = document.getElementById('sell-coupon');
+    if(text === couponText.innerText)
+    {
+        applyBtn.removeAttribute('disabled');
+        applyBtn.addEventListener('click',function(){
+            console.log('20% korte hobe');
+        })
+    }
+    else{
+        applyBtn.setAttribute('disabled',true);
+    }
+})
